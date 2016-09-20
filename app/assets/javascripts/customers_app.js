e891acb733ed0d45f38796148666d679efb19083
michaeldumalag <michaeldumalag@gmail.com>
2016-09-19 21:08:58 -0700

    Porting search with angular and canned results

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,7 +1,35 @@
 var app = angular.module('customers', []);
 
-var CustomerSearchController = function($scope){
-  $scope.search = function(searchTerm) {
-    $scope.searchedFor = searchTerm;
+app.controller("CustomerSearchController", [
+          "$scope",
+  function($scope) {
+    $scope.customers = [];
+    $scope.search = function(searchTerm) {
+      $scope.customers = [
+        {
+          "first_name":"Schuyler",
+          "last_name":"Cremin",
+          "email":"giles@macguyver.net",
+          "username":"jillian0",
+          "created_at":"2015-03-04",
+        },
+        {
+          "first_name":"Derick",
+          "last_name":"Ebert",
+          "email":"lupel1@rennerfisher.org",
+          "username":"ubaldo_kaulkel",
+          "created_at":"2015-03-04",
+        },
+        {
+          "first_name":"Derick",
+          "last_name":"Johnsons",
+          "email":"dj@somewhere.org",
+          "username":"djj",
+          "created_at":"2015-03-04",
+        }
+      ]
+
+      $scope.searchedFor = searchTerm;
+    }
   }
-}
+]);
