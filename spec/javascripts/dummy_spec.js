3802338445db8518534fa909066278dd5e8b7a4d
michaeldumalag <michaeldumalag@gmail.com>
2016-09-25 21:38:15 -0700

    Setup teaspoon gem for jasmine and attempted a dummy test

---
+++ spec/javascripts/dummy_spec.js
@@ -0,0 +1,6 @@
+// This is a dummy spec file to test if teaspoon and jasmine work
+describe("Testing Jasmine", function() {
+  it("can run a test", function() {
+    expect(true).toBe(true);
+  });
+});
